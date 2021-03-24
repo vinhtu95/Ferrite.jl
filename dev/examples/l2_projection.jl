f24c88b393c40263d9f90eeb9c2c7c073c9155a5
Documenter.jl <documenter@juliadocs.github.io>
2021-03-24 12:44:01 +0000

    build based on d04709f

--- dev/examples/l2_projection.jl
+++ dev/examples/l2_projection.jl
@@ -9,25 +9,25 @@ function compute_heat_fluxes(cellvalues::CellScalarValues{dim,T}, dh::DofHandler
     # Allocate storage for the fluxes to store
     q = [Vec{2,T}[] for _ in 1:getncells(dh.grid)]
 
-    @inbounds for (cell_num, cell) in enumerate(CellIterator(dh))
+    for (cell_num, cell) in enumerate(CellIterator(dh))
         q_cell = q[cell_num]
         celldofs!(cell_dofs, dh, cell_num)
         aᵉ = a[cell_dofs]
         reinit!(cellvalues, cell)
 
         for q_point in 1:nqp
-            q_qp = -function_gradient(cellvalues, q_point, aᵉ)
+            q_qp = - function_gradient(cellvalues, q_point, aᵉ)
             push!(q_cell, q_qp)
         end
     end
     return q
 end
 
-q = compute_heat_fluxes(cellvalues, dh, u);
+q_gp = compute_heat_fluxes(cellvalues, dh, u);
 
 projector = L2Projector(ip, grid);
 
-q_nodes = project(projector, q, qr);
+q_nodes = project(projector, q_gp, qr);
 
 vtk_grid("heat_equation_flux", grid) do vtk
     vtk_point_data(vtk, q_nodes, "q")
